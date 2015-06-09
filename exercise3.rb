65856d378541be233b7d80da1bd7e71857fbd9cc
Bryan <brymac@gmail.com>
2015-06-09 14:34:56 -0400

    Added date of birth and age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi, #{name}"
+puts "How old are you?"
+age = gets.chomp.to_i
+current_time = Time.new
+birth_year = current_time.year.to_i - age
+puts "You were born in #{birth_year} if your birth day is before #{current_time.month.to_s}-#{current_time.day.to_s}.  Otherwise you were born in #{(birth_year - 1).to_s}"
