948de85bdb465859eba6e308ed1f9651b454bc12
Bryan <brymac@gmail.com>
2015-06-09 14:51:26 -0400

    Updated exercise 3 with better date format

--- exercise3.rb
+++ exercise3.rb
@@ -3,6 +3,6 @@ name = gets.chomp
 puts "Hi, #{name}"
 puts "How old are you?"
 age = gets.chomp.to_i
-current_time = Time.new
+current_time = Time.now
 birth_year = current_time.year.to_i - age
-puts "You were born in #{birth_year} if your birth day is before #{current_time.month.to_s}-#{current_time.day.to_s}.  Otherwise you were born in #{(birth_year - 1).to_s}"
+puts "You were born in #{birth_year} if your birth day is before #{current_time.strftime("%B")} #{current_time.day.to_s}.  Otherwise you were born in #{(birth_year - 1).to_s}"
